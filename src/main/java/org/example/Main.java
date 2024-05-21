3a779ccf60a973751318728a25e89b7759a37b50
Jakub Indrak <hexaplay2@gmail.com>
2024-05-21 09:09:34 +0200

    Interface and Dependency Injection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       SceneManager sm = new SceneManager();
+        sm.loop();
     }
 }
